f27be846511e9456d2ea00c02e92ea66156f8f0c
satork <sebastian.kedzier@gmail.com>
2024-02-06 18:40:05 +0100

    script.js polishing added - packed logs to last playGame function

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 // Scissors-1
 // Rock-2
 
-
 //PROMT OF HOW MANY ROUNDS
-let rounds = prompt("How many rounds do you want to play?")
+let rounds = prompt("How many rounds do you want to play?");
 
 //GLOBAL VARIABLES FOR SCISSORS AND WIN COUNTS
 const paperScissorsRock = ["Paper |", "Scissors ܓ", "Rock O"];
@@ -19,42 +18,56 @@ const getComputerChoice = () => {
 
 //PLAYER CHOICE
 const getPlayerSelection = () => {
-let playerSelection;
-let bon = 0;
-do {
-  playerSelection = prompt("Paper(|), Scissors(ܓ) or Rock(O)?:");
-  if (playerSelection.toLocaleLowerCase() == "paper") {
-    bon = 1;
-    playerSelection = paperScissorsRock[0];
-  } else if (playerSelection.toLocaleLowerCase() == "scissors") {
-    bon = 1;
-    playerSelection = paperScissorsRock[1];
-  } else if (playerSelection.toLocaleLowerCase() == "rock") {
-    bon = 1;
-    playerSelection = paperScissorsRock[2];
-  }
-} while (bon < 1);
-return playerSelection
-}
+  let playerSelection;
+  let bon = 0;
+  do {
+    playerSelection = prompt("Paper(|), Scissors(ܓ) or Rock(O)?:");
+    if (playerSelection.toLocaleLowerCase() == "paper") {
+      bon = 1;
+      playerSelection = paperScissorsRock[0];
+    } else if (playerSelection.toLocaleLowerCase() == "scissors") {
+      bon = 1;
+      playerSelection = paperScissorsRock[1];
+    } else if (playerSelection.toLocaleLowerCase() == "rock") {
+      bon = 1;
+      playerSelection = paperScissorsRock[2];
+    }
+  } while (bon < 1);
+  return playerSelection;
+};
 
 //ONE ROUND
 function playRound(computerSelection, playerSelection) {
   if (computerSelection == playerSelection) {
     console.log("It is a draw!");
-  } else if (computerSelection == paperScissorsRock[0] && playerSelection == paperScissorsRock[1]) {
+  } else if (
+    computerSelection == paperScissorsRock[0] &&
+    playerSelection == paperScissorsRock[1]
+  ) {
     console.log("Player Wins");
     pWinCount++;
-  } else if (computerSelection == paperScissorsRock[0] &&
-    playerSelection == paperScissorsRock[2]) {
+  } else if (
+    computerSelection == paperScissorsRock[0] &&
+    playerSelection == paperScissorsRock[2]
+  ) {
     console.log("Computer Wins");
     cWinCount++;
-  } else if (computerSelection == paperScissorsRock[1] && playerSelection == paperScissorsRock[0]) {
+  } else if (
+    computerSelection == paperScissorsRock[1] &&
+    playerSelection == paperScissorsRock[0]
+  ) {
     console.log("Computer Wins");
     cWinCount++;
-  } else if (computerSelection == paperScissorsRock[1] && playerSelection == paperScissorsRock[2]) {
+  } else if (
+    computerSelection == paperScissorsRock[1] &&
+    playerSelection == paperScissorsRock[2]
+  ) {
     console.log("Player Wins");
     pWinCount++;
-  } else if (computerSelection == paperScissorsRock[2] && playerSelection == paperScissorsRock[0]) {
+  } else if (
+    computerSelection == paperScissorsRock[2] &&
+    playerSelection == paperScissorsRock[0]
+  ) {
     console.log("Player Wins");
     pWinCount++;
   } else {
@@ -67,13 +80,19 @@ function playRound(computerSelection, playerSelection) {
 const playGame = () => {
   for (let i = 0; i < rounds; i++) {
     let computerSelection = getComputerChoice();
-    let playerSelection =  getPlayerSelection();
+    let playerSelection = getPlayerSelection();
     console.log("Computer: " + computerSelection);
     console.log("Player: " + playerSelection);
-    playRound(computerSelection,playerSelection)
+    playRound(computerSelection, playerSelection);
   }
-}
+  console.log(cWinCount + " " + pWinCount);
+  cWinCount > pWinCount
+    ? console.log("Computer is Ultimate Winner")
+    : cWinCount == pWinCount
+    ? console.log("Draw Game")
+    : cWinCount < pWinCount
+    ? console.log("Player is Ultimate Winner")
+    : console.log("Computer is Ultimate Winner");
+};
 
-playGame()
-console.log(cWinCount + " " + pWinCount);
-cWinCount > pWinCount ? console.log("Computer is Ultimate Winner") : cWinCount == pWinCount ? console.log("Draw Game") : cWinCount < pWinCount ? console.log("Player is Ultimate Winner") : console.log("Computer is Ultimate Winner");
+playGame();
